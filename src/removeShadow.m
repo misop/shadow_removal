d7bc04807029e2950f10bb2e2cd88163e9163af8
Matej <Kopernick>
2013-05-08 03:00:10 +0200

    pokus o lepsie okraje tiena
    dost nepodareny pokus, vyzera to vacsinou horsie ako predtym
    pokusy s HSV boli este horsie, tie som hned zmazal
    momentalne ma uz nenapada, ako to spravit lepsie a nie prilis zlozito

--- src/removeShadow.m
+++ src/removeShadow.m
@@ -1,4 +1,4 @@
-function removeShadow(I, mask)
+function removeShadow(I, mask, smoothEdges)
 
 comp = bwconncomp(mask,8);
 %odstranime kazdy tien (jeden spojity komponent binarnej masky)
@@ -37,15 +37,17 @@ for i=1:comp.NumObjects
         t(j) = atan2(sy, sx);
     end
     
-    I = remove(I, maskTmp, x, y, t);
+    I = remove(I, maskTmp, x, y, t, smoothEdges);
 end
 
+figure; imshow(I);
+
 end
 
 
 
 
-function ImgOut = remove(ImgIn, mask, x, y, t)
+function ImgOut = remove(ImgIn, mask, x, y, t, smoothEdges)
 
 ps = 8;
 Id = double(ImgIn) ./ 255;
@@ -56,7 +58,8 @@ Id = double(ImgIn) ./ 255;
 npt = length(x);
 aCh = double([0.0, 0.0, 0.0]);
 aChs = double([0, 0, 0]);
-for n = 1:npt   
+
+for n = 1:npt
     x0 = x(n); y0 = y(n); t0 = t(n);
     x1 = round(x0 + 1.5*ps*cos(t0+pi/2));
     y1 = round(y0 + 1.5*ps*sin(t0+pi/2));
@@ -80,6 +83,7 @@ for n = 1:npt
 end
 aCh = aCh ./ npt;
 npt = 0;
+
 for i = 1:h
     for j = 1:w
         if (mask(i, j) == 1)
@@ -93,6 +97,7 @@ end
 aChs = aChs ./ npt;
 sl = zeros(h, w, 3);
 chs = aCh ./ aChs;
+
 for i = 1:h
     for j = 1:w
         if (mask(i, j) == 1)
@@ -104,8 +109,63 @@ for i = 1:h
         end
     end
 end
+
+
+%skusme odstranit hranu medzi tienom a svetlom (znie to trochu poeticky)
+%prd skusime, nefunguje to
+if (smoothEdges)
+    for n = 1:length(x)
+        for j=-2:2
+            for k=-2:2
+                
+                x0 = x(n)+j; y0 = y(n)+k; t0 = t(n);
+                if (x0+j < 1 || x0+j > h || y0+k < 1 || y0+k > w)
+                    continue;
+                end
+                x1 = round(x0 + j + 1.5*ps*cos(t0+pi/2));
+                y1 = round(y0 + k + 1.5*ps*sin(t0+pi/2));
+                x2 = round(x0 + j + 1.5*ps*cos(t0-pi/2));
+                y2 = round(y0 + k + 1.5*ps*sin(t0-pi/2));
+                avg = [0.0 0.0 0.0];
+                avgCnt = 0;
+                avgs = [0.0 0.0 0.0];
+                avgsCnt = 0;
+                for j1=-2:2
+                    for k1=-2:2
+                        for ch=1:3
+                            if (x1+j1 > 0 && x1+j1 <= h  && y1+k1 > 0 && y1+k1 <= w)
+                                if (mask(x1+j1,y1+k1)==0)
+                                    avg(ch) = avg(ch) + double(ImgIn(x1+j1,y1+k1,ch));
+                                    avgCnt = avgCnt + 1;
+                                else
+                                    avgs(ch) = avgs(ch) + double(ImgIn(x1+j1,y1+k1,ch));
+                                    avgsCnt = avgsCnt + 1;
+                                end
+                            end
+                            if (x2+j1 > 0 && x2+j1 <= h  && y2+k1 > 0 && y2+k1 <= w)
+                                if (mask(x2+j1,y2+k1)==0)
+                                    avg(ch) = avg(ch) + double(ImgIn(x2+j1,y2+k1,ch));
+                                    avgCnt = avgCnt + 1;
+                                else
+                                    avgs(ch) = avgs(ch) + double(ImgIn(x2+j1,y2+k1,ch));
+                                    avgsCnt = avgsCnt + 1;
+                                end
+                            end
+                        end
+                    end
+                end
+                
+                if avgCnt>0 && avgsCnt>0
+                    for ch=1:3
+                        sl(x0,y0,ch) = avg(ch)/avgCnt - avgs(ch)/avgsCnt + ImgIn(x0,y0,ch);
+                    end
+                end
+            end
+        end
+    end
+end
+
 sl = uint8(sl);
-figure; imshow(sl);
 ImgOut = sl;
 end
 
